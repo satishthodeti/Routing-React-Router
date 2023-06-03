561a078dc13d23291b23bb4520c3d6ea4dabeb81
satishthodeti <satishthodeti39gamail.com>
2023-06-03 13:16:47 +0000

    Solution Code

---
+++ src/components/Contact/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact-img"
+    />
+    <h1 className="contact-heading">Contact</h1>
+    <p className="about-paragraph">Mobile: 9133833066</p>
+    <a href="https://www.linkedin.com/in/satishthodeti/">
+      https://www.linkedin.com/in/satishthodeti/
+    </a>
+  </div>
+)
+
+export default Contact
---
+++ src/components/Header/index.js
@@ -0,0 +1,43 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="header-container">
+    <div className="logo-and-title-container">
+      <a
+        href="https://routingsatish.ccbp.tech/"
+        rel="noreferrer"
+        target="_self"
+      >
+        <img
+          href="https://satishrouting.ccbp.tech/"
+          alt="wave"
+          className="logo"
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        />
+        {/* <h1 className="title">Wave</h1> */}
+      </a>
+    </div>
+
+    <ul className="nav-items-list">
+      <li className="link-item">
+        <Link className="route-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
